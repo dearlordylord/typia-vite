4f86694eb83fffdbbce8f179b373aad868a5f88c
Igor Loskutov <igor.loskutoff@gmail.com>
2024-07-01 11:31:46 -0400

    vite plugin inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnpluginTypia from '@ryoppippi/unplugin-typia/vite'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [UnpluginTypia({
     log: 'verbose',
     tsconfig: './tsconfig.app.json',
-  }), react()],
+  }), react(), Inspect()],
 })
